61533e30c52ece4a975a131571560049f2a2f1f4
Harshit Joshi <57838029+Hellsender01@users.noreply.github.com>
2022-01-06 23:06:54 +0530

    Created ret2plt.py

---
+++ pwnlib/rop/ret2plt.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+from __future__ import division
+
+from ..log import getLogger
+
+log = getLogger(__name__)
+
+
+def ret2plt(rop, elf, leak, ret=None):
+    """Create ret2plt ROP chain
+
+    Args:
+        elf (pwnlib.elf.elf.ELF): ELF Object of binary
+        leak (int): Function address to leak from GOT
+        ret (int,None): Address to return
+    """
+
+    if "puts" not in elf.got:
+        log.error("Fuction puts not found in GOT.")
+        return None
+
+    if leak not in elf.got.values():
+        log.error("No such address to leak in GOT.")
+
+    if ret:
+        if ret not in elf.sym.values():
+            log.error("No such address to leak in GOT.")
+
+    rop.puts(leak)
+
+    if ret:
+        rop.call(ret)
